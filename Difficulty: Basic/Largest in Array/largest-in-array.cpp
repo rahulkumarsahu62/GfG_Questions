8cbb828f94f85c051b028e567449407dc8be0923
Rahul Kumar Sahu <rahulkrsahu62@gmail.com>
2026-09-01 18:53:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Largest in Array/largest-in-array.cpp
@@ -0,0 +1,9 @@
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        // code here
+         int n = arr.size();
+        sort(arr.begin(),arr.end());
+        return arr[n-1];
+    }
+};
